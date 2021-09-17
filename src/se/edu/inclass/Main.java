83acbd6bb4ed384dadbea191bcff4c8e8d421ef5
markuslyq <markuslyq@gmail.com>
2021-09-17 17:54:24 +0800

    Merge branch 'master' into lambdas

--- src/se/edu/inclass/Main.java
+++ src/se/edu/inclass/Main.java
@@ -23,6 +23,10 @@ public class Main {
         printDeadlinesUsingStream(tasksData);
         ArrayList<Task> filteredList = filterTasksByString(tasksData, "11");
         printData(filteredList);
+        printDeadlinesUsingStream(tasksData);
+
+        System.out.println("Total number of deadlines: " + countDeadlinesUsingStream(tasksData));
+
     }
 
     private static int countDeadlines(ArrayList<Task> tasksData) {
@@ -35,10 +39,23 @@ public class Main {
         return count;
     }
 
+    private static int countDeadlinesUsingStream(ArrayList<Task> tasks) {
+        return (int) tasks.stream()
+                .filter((t) -> t instanceof Deadline)
+                .count();
+    }
+
     public static void printData(ArrayList<Task> tasksData) {
+        System.out.println("Printing data by looping: ");
         for (Task t : tasksData) {
             System.out.println(t);
         }
+    }
+
+    public static void printDataWithStreams(ArrayList<Task> tasks) {
+        System.out.println("Printing data using stream: ");
+        tasks.stream() // convert data to stream
+                .forEach(System.out::println); // terminal operator
     }
 
     public static void printDeadlines(ArrayList<Task> tasksData) {
@@ -61,4 +78,5 @@ public class Main {
                 .filter((t) -> t.getDescription().contains(filterString))
                 .collect(Collectors.toList());
     }
+
 }
